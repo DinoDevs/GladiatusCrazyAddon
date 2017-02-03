286d53caa22fb688ffbab20c830baa4cfb1ae3ec
GramThanos <darkthanos.gr@gmail.com>
2017-02-03 23:54:16 +0200

    Oups forgot to return

--- source/core/source/global.js
+++ source/core/source/global.js
@@ -1096,6 +1096,7 @@ var gca_global = {
 					if(gold == 0 || isNaN(gold)){
 						// Show warning
 						gca_notifications.warning( gca_locale.get("no_gold") );
+						return;
 					}
 
 					var that = this;
