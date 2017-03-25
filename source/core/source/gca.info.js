4a567ac9ae11cd4dd4456ec9a731fa89d26440c2
GramThanos <darkthanos.gr@gmail.com>
2017-03-25 04:28:00 +0200

    Removed old url compartibility

--- source/core/source/gca.info.js
+++ source/core/source/gca.info.js
@@ -21,37 +21,13 @@ var gca_section = {
 	// Resolve Url
 	resolve : function(){
 		var url = document.location.href;
-		
-		// Old Url
-		if(url.match(/s\d+\.\w+\.gladiatus\.gameforge\.com/)){
-			this.country = (url.match(/\.(\w*)\.gladiatus\.gameforge\.com/))?url.match(/\.(\w*)\.gladiatus\.gameforge\.com/)[1]:null;
-			this.server = (url.match(/s\d+\./i))?url.match(/s(\d+)\./i)[1]:null;
-			this.urlMode = "old";
-		}
-		
-		// New Url
-		else{
-			this.country = (url.match(/s\d+-(\w*)\.gladiatus\.gameforge\.com/))?url.match(/s\d+-(\w*)\.gladiatus\.gameforge\.com/)[1]:null;
-			this.server = (url.match(/s\d+-/i))?url.match(/s(\d+)-/i)[1]:null;
-			this.urlMode = "new";
-		}
-		
+		this.country = (url.match(/s\d+-(\w*)\.gladiatus\.gameforge\.com/))?url.match(/s\d+-(\w*)\.gladiatus\.gameforge\.com/)[1]:null;
+		this.server = (url.match(/s\d+-/i))?url.match(/s(\d+)-/i)[1]:null;
 		this.mod = (url.match(/mod=\w+/i))?url.match(/mod=(\w+)/i)[1]:null;
 		this.submod = (url.match(/submod=\w+/i))?url.match(/submod=(\w+)/i)[1]:null;
 		this.gcamod = (url.match(/gcamod=\w+/i))?url.match(/gcamod=(\w+)/i)[1]:null;
 		this.sh = (url.match(/sh=[0-9a-fA-F]+/i))?url.match(/sh=([0-9a-fA-F]+)/i)[1]:null;
 		this.domain = document.domain;
-
-		/*
-		var url = document.location.href;
-		this.country = (url.match(/\.(\w*)\.gladiatus\.gameforge\.com/)[1])?url.match(/\.(\w*)\.gladiatus\.gameforge\.com/)[1]:null;
-		this.server = (url.match(/s\d+\./i))?url.match(/s(\d+)\./i)[1]:null;
-		this.mod = (url.match(/mod=\w+/i))?url.match(/mod=(\w+)/i)[1]:null;
-		this.submod = (url.match(/submod=\w+/i))?url.match(/submod=(\w+)/i)[1]:null;
-		this.gcamod = (url.match(/gcamod=\w+/i))?url.match(/gcamod=(\w+)/i)[1]:null;
-		this.sh = (url.match(/sh=[0-9a-fA-F]+/i))?url.match(/sh=([0-9a-fA-F]+)/i)[1]:null;
-		this.domain = document.domain;
-		*/
 
 		this.resolvePlayerId();
 	},
