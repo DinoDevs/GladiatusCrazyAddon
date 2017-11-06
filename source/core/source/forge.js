06df88521b7ff6a355d1c2d6bd9e21650c98570b
Grammatopoulos Apostolos <greatapo@gmail.com>
2017-11-06 02:30:49 +0200

    Required Resources Links
    - Only on workbench
    - Bug with UTF-8 on same page reload

--- source/core/source/forge.js
+++ source/core/source/forge.js
@@ -17,8 +17,10 @@ var gca_forge = {
 		}else if(gca_section.submod=='smeltery'){
 			this.saveSmeltTimers();
 		//Repair
-		}else{
+		}else{//workbench
 			// TODO
+			
+			this.sourceLinks();
 		}
 
 	},
@@ -35,6 +37,44 @@ var gca_forge = {
 			}
 			gca_data.section.set("timers", "smelt_times", smeltTimes);
 		}
+	},
+	
+	sourceLinks : function(){
+		if(document.getElementsByClassName('crafting_requirements').length>0 && typeof document.getElementById('forge_nav').getElementsByClassName('tabActive')[0]!=="undefined"){
+			var tab = document.getElementById('forge_nav').getElementsByClassName('tabActive')[0].className.match(/forge_(opened|closed) (\d) /i)[2];
+			
+			if(typeof document.getElementsByClassName('crafting_requirements')[0].dataset.runlinks=="undefined")
+				document.getElementsByClassName('crafting_requirements')[0].dataset.runlinks=-1;
+			
+			if(document.getElementsByClassName('crafting_requirements')[0].dataset.runlinks!==tab){
+				document.getElementsByClassName('crafting_requirements')[0].dataset.runlinks=tab;
+				
+				var li = document.getElementsByClassName('crafting_requirements')[0].getElementsByTagName('li');
+				var name,linkBox;
+				var links=[];
+				for(var i=0;i<li.length;i++){
+					name = encodeURIComponent(li[i].getElementsByTagName('div')[0].title);
+					linkBox = document.createElement('div');
+					linkBox.className = 'forge_amount';
+					linkBox.style = 'background-color: #bba86e;font-size: 12px;';
+					li[i].appendChild(linkBox);
+					links[0] = document.createElement('a');
+					links[0].href = gca_getPage.link({"mod":"packages","qry":name});
+					links[0].textContent = '⧉ ';
+					links[0].style = "text-decoration:none;";
+					linkBox.appendChild(links[0]);
+					links[1] = document.createElement('a');
+					links[1].href = gca_getPage.link({"mod":"market","qry":name});
+					links[1].textContent = ' ⚖';
+					links[1].style = "text-decoration:none;";
+					linkBox.appendChild(links[1]);
+				}
+			}
+		}
+		
+		setTimeout(function(){
+			gca_forge.sourceLinks();
+		}, 500);
 	}
 };
 
