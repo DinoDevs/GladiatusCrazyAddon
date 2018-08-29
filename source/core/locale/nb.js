755125eaeefd152eebac307d53d3182daa385a7c
GramThanos <darkthanos.gr@gmail.com>
2018-08-29 12:42:15 +0300

    Norwegian translations updated

--- source/core/locale/nb.js
+++ source/core/locale/nb.js
@@ -3,7 +3,7 @@
  * Name : Norwegian
  * Code : [none]
  * Tag  : nb
- * Translator: Rune1 (Norway)
+ * Translator: Rune1 (Norway), Raffert1 [gladiatusraffert@hotmail.no]
  */
 
 // Languages Object
@@ -12,17 +12,19 @@ var gca_languages = gca_languages || {};
 // Set Language
 gca_languages['nb'] = {
 	name : 'Norsk (Norwegian)',
-	translators : ["Rune1"],
+	translators : ["Rune1", "Raffert1"],
 	locale : {
 		info : {
 			description : "Den villeste utvidelsen til Gladiatus noensinne!"
 		},
 		general : {
-			days : "dager",
+			days : "dag(er)",
 			minutes : "minutter",
-			no_data : "det er ikke noe data/innformasjon",
+			no_data : "det er ikke noe data/informasjon",
+			confirm : "Bekreft",
+			cancel : "Angre",
 			close : "lukk",
-			error : "error"
+			error : "feil"
 		},
 		global : {
 			life_recover_full : "fult liv innen",
@@ -34,7 +36,7 @@ gca_languages['nb'] = {
 			message_empty : "klan melding er tom",
 			message_exclude_me : "ekskluder meg",
 			guild_market_goto : "gå til klanens marked",
-			guild_storage_goto : "gå til klanens lagring",
+			guild_storage_goto : "gå til klanens lagerhus",
 			guild_bank_goto : "gå til klanens bank",
 			guild_warcamp_goto : "gå til klanens krigsleir",
 			guild_jail_goto : "gå til klanens fengsel",
@@ -46,36 +48,78 @@ gca_languages['nb'] = {
 			online_friends : "påloggede venner",
 			guild_friends : "klan venner",
 			family_friends : "familie venner",
-			donate_gold_success : "dit gull er donert",
-			donate_gold_failed : "din gull donering feilet",
-			donate_gold_all_gold : "doner alt dit gull",
-			quest_full : "fult",
-			quest_new : "ny",
+			donate_gold_confirm : "Ønsker du å donere {{number}} gull?",
+			donate_gold_success : "Du har donert",
+			donate_gold_failed : "Gull donering feilet",
+			donate_gold_no_gold : "Du har ikke noe gull å donere",
+			donate_gold_all_gold : "Doner alt gullet ditt",
+			quest_full : "Fullt",
+			quest_new : "Ny",
+			pray_start : "Trykk for å starte bønn",
+			pray_stop : "Trykk for å stoppe bønn",
+			heal : "helbred",
 			notification_guild_application : "det er en uavgjort klan applikasjon",
-			gold_exp_data_to_level_up : "dager til du rangerer opp"
+			gold_exp_data_to_level_up : "dager til du rangerer opp",
+			gold_exp_data_today : "Siste 24 timer",
+			gold_exp_data_week : "Siste 7 dager",
+			gold_exp_data_to_level_up : "Dager til du øker i nivå",
+			gold_exp_data_total_exp : "Total erfaring",
+			gold_exp_data_total_gold : "Totalt gull"
 		},
-		overview : {},
-		pantheon : {},
+		overview : {
+			stats_difference : "Forskjell",
+			drop_item_see_materials_repair : "Dra en gjenstand for å se hva du trenger for å reparere den"
+		},
+		pantheon : {
+			mysterybox_open_all : "Åpne alle",
+			mysterybox_open_stop : "Stopp",
+			mysterybox_open_done : "Ferdig!"
+		},
 		guild : {
-			bank_all_gold : "legg til alt gullet",
-			medic_lost_points : "mistet poeng",
+			bank_all_gold : "Alt gullet ditt",
+			medic_lost_points : "Tapt poeng",
 			medic_points_to_heal : "poeng til helbreding",
 			medic_life_after_heal : "liv etter helbreding"
 		},
-		training : {},
+		expedition : {
+		},
+		training : {
+			total_cost : "Total kostnad",
+			costs_discount : "Treningskostnader rabatt: {{number}}%"
+		},
 		auction : {
 			number_of_items : "antall gjenstander : {{number}}",
 			number_of_bided_items : "antall bydde gjenstander : {{number}}",
 			hide_your_gold_here : "gjem gullet dit her",
 			price_value_function : "pris = verdi + {{number}}"
 		},
+		markets : {
+			item_is_soulbound : "Denne gjenstanden er sjelbundet",
+			are_you_sure_you_want_to_buy : "Vil du virkelig kjøpe denne gjenstanden?"
+		},
+		forge : {
+		},
+		packages : {
+			known_scroll : "Du har lært denne skriftrullen",
+			unknown_scroll : "Du har ikke lært denne skriftrullen"
+		},
 		settings : {
-			settings : "Instillinger",
+			settings : "Innstillinger",
 			description : "Aktiver eller deaktiver de forskjellige utvidelsene du vil ",
-			description_click_button : "Trykk på knappen under for å gå til utvidelse instillinger",
-			category_global : "Generelle instillinger",
-			category_overview : "Oversikts instillinger",
-			category_global$language_select : "Bytt utvidelse språk",
+			description_click_button : "Trykk på knappen under for å gå til utvidelse innstillinger",
+			category_global : "Generelle innstillinger",
+			category_overview : "Oversikt",
+			category_messages : "Beskjeder",
+			category_packages : "Pakker",
+			category_reports : "Rapporter",
+			category_training : "Trening",
+			category_arena : "Arena",
+			category_market : "Marked",
+			category_expedition : "Ekspedisjon",
+			category_guild : "Klan",
+			category_auction : "Auksjon",
+			category_sound : "Lyder",
+			category_global$language_select : "Velg utvidelse språk",
 			category_global$sound_notifications : "Aktiver lyd varsling for oppdrag, fangehule og arenaer",
 			category_global$extended_hp_xp_info : "Vis ekstra helse- og erfarings poeng hoved info",
 			category_global$auction_status_bar : "Vis auksjons status indikator",
@@ -83,7 +127,13 @@ gca_languages['nb'] = {
 			category_global$remember_tabs : "Husk kjøpmanns og lagrings faner",
 			category_global$merchants_timer : "Vis kjøpmenns tid",
 			category_overview$analyze_items : "Analyser spillers ting",
-			save : "Lagre alle "
+			category_pantheon$missions_show_completed : "Vis fullførte oppdrag",
+			category_training$show_discount : "Vis treningsrabatt",
+			category_sound$volume : "Lydnivå",
+			save : "Lagre",
+			reset : "Nullstill",
+			clear : "Tøm",
+			do_not_show : "Ikke vis"
 		}
 	}
 }
