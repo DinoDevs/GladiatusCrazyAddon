842b90468dd9c6c6cb6c46e707376b701e824e24
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2018-11-13 20:11:16 +0200

    Version number was set to 4.1.0

--- source/core/source/gca.info.js
+++ source/core/source/gca.info.js
@@ -8,7 +8,7 @@ var gca = {
 	name : "Gladiatus Crazy Addon",
 	shortName : "GCA",
 	description: "Improve your gladiatus experience!",
-	version: "4.0.6",
+	version: "4.1.0",
 	developers : [
 		"GreatApo",
 		"DarkThanos"
