80f84f026a9dcb9ec5ddddfadeb237ca7b4a6a7d
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2020-03-20 22:06:40 +0200

    Ported double-click to guild storage

--- source/core/source/guild.storage.js
+++ source/core/source/guild.storage.js
@@ -7,8 +7,12 @@
 var gca_guild_storage = {
 	inject : function(){
 		// If Item shadow
-		(gca_options.bool("global","item_shadow") && 
+		(gca_options.bool("global","item_shadow") &&
 			this.itemShadow.inject());
+
+		// Double click to sell/buy items
+		(gca_options.bool("merchants","double_click_actions") &&
+			this.doubleClickActions.init());
 
 		// Setting Link
 		gca_tools.create.settingsLink("guild");
@@ -31,6 +35,66 @@ var gca_guild_storage = {
 			}
 		}
 	},
+
+	// Double click sell/buy
+	doubleClickActions : {
+		init : function(){
+			// Apply item events
+			this.apply();
+
+			// Add event
+			gca_tools.event.bag.onBagOpen(() => {
+				this.apply();
+			});
+
+			// If bag not already loaded
+			if (document.getElementById("inv").className.match("unavailable")) {
+				// Wait first bag
+				gca_tools.event.bag.waitBag(() => {
+					this.apply();
+				});
+			}
+
+			// On item move
+			gca_tools.event.request.onAjaxResponse((data) => {
+				if (
+					data.hasOwnProperty("data") && data.data &&
+					data.data.hasOwnProperty("to") && data.data.to &&
+					data.data.to.hasOwnProperty("data") && data.data.to.data &&
+					data.elem.length === 1
+				) {
+					let item = jQuery('#content .ui-draggable[data-hash=' + data.elem[0].dataset.hash + ']');
+					if (item) delete item[0].dataset.gcaFlag_doubleClickEvent;
+					this.apply();
+				}
+			});
+		},
+		apply : function(){
+			this.applyOn(jQuery('#inv .ui-draggable'));
+			this.applyOn(jQuery('#shop .ui-draggable'));
+		},
+		applyOn : function(items){
+			var that = this;
+			// For each
+			items.each(function(){
+				if (!this.dataset) return;
+				// If already parsed
+				if(this.dataset.gcaFlag_doubleClickEvent) return;
+				// Flag as parsed
+				this.dataset.gcaFlag_doubleClickEvent = true;
+				// Add event
+				this.addListener('dblclick', that.handler);
+			});
+		},
+		handler : function() {
+			if (this.parentNode.id == 'inv') {
+				gca_tools.item.move(this, 'shop');
+			}
+			else if (this.parentNode.id == 'shop') {
+				gca_tools.item.move(this, 'inv');
+			}
+		}
+	}
 };
 
 // Onload Handler
@@ -48,3 +112,7 @@ var gca_guild_storage = {
 		window.addEventListener('load', fireLoad, true);
 	}
 })();
+
+// ESlint defs
+/* global gca_options, gca_tools */
+/* global jQuery */
