8d45a8338c91995a2cacf3a3d059d05cc9ab08d7
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2020-06-16 22:57:20 +0300

    Added missing translations as comment

--- source/core/locale/cs.js
+++ source/core/locale/cs.js
@@ -22,6 +22,7 @@ gca_languages['cs'] = {
 		general : {
 			days : "dny",
 			minutes : "minut",
+//hours : "hours",
 			no_data : "Nejsou vyplněny žádná hodnoty",
 			
 			// Buttons
@@ -360,6 +361,19 @@ gca_languages['cs'] = {
 			category_merchants$fade_unaffordable_items : "Nechat vyblednout předměty,na které nemám peníze",
 			category_merchants$show_shop_info : "Zobrazit info obchodu (celkové zlato a rubíny)",
 			category_merchants$double_click_actions : "Dvojklik pro prodat/koupit",
+			// Settings - Forge
+//category_forge$material_links : "[Forge/Repair] Show packages & market shortcuts for each material need",
+//category_forge$show_levels : "[Forge] Show Prefix/Sufix/Base item levels next to names",
+//category_forge$horreum_materials_names : "[Horreum] Show material name",
+//category_forge$horreum_remember_options : "[Horreum] Remember last selected store settings",
+//category_forge$horreum_select_meterials : "[Horreum] Select material on click",
+			// Settings - Arena
+//category_arena$ignore_attack_confirmations : "Ignore attack confirmations (over 5 attacks message etc)",
+//category_arena$show_simulator_imagelink : "Show an image-link to the simulator (simulator.dinodevs.com)",
+//category_arena$sort_by_lvl : "Sort players in arena by level",
+//category_arena$highlight_guild_members : "Highlight players on other servers that may be guild members",
+//category_arena$target_list : "Players target list",
+
 			// Settings - Magus
 			category_magus$fade_unimprovable_items : "Nechat vyblednout předměty, které nelze zdokonalit",
 			// Settings - Market
