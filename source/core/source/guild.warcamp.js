f21d0455db3b7e5dc10d7a54524ab760e1f1eaee
Grammatopoulos Apostolos <greatapo@gmail.com>
2020-07-07 23:18:20 +0300

    Guild war camp: added gladiatorius links

--- source/core/source/guild.warcamp.js
+++ source/core/source/guild.warcamp.js
@@ -20,37 +20,54 @@ var gca_guild_warcamp = {
 	guildAttack : {
 		// Add direct attack button
 		directAttack : function(){
-			
 			// Parse each guild
 			jQuery('.section-like:eq(0) tr').each((index, element) => {
 				
 				if (element.getElementsByTagName("a").length < 2){
 					element.appendChild(document.createElement("th"));
+					let th = document.createElement("th");
+					th.textContent = "Gladiatorius";
+					element.insertBefore(th, element.getElementsByTagName("th")[1]);
 					return;
 				}
 				
-				// Get name
-				let guildID = element.getElementsByTagName("a")[0].href.trim().match(/i=(\d+)/i)[1];
-				let link = element.getElementsByTagName("a")[1].href;
+				// Get info
+				let guildName = element.getElementsByTagName("a")[0].textContent.trim();
+				let guild_link = element.getElementsByTagName("a")[0].href;
+				//let guildID = guild_link.trim().match(/i=(\d+)/i)[1];
+				let attack_link = element.getElementsByTagName("a")[1].href;
+				let life_points_translation = jQuery('#header_values_hp_bar').data("tooltip")[0][0][0][0].trim().replace(":","");
 				
 				// Add direct attack button
 				let td = document.createElement("td");
 				let form = document.createElement("form");
-				form.action = link;
+				form.action = attack_link;
 				form.method = "POST";
 				let input = document.createElement("input");
 				input.type = "submit";
 				input.name = "combat";
 				input.className = "gca_guild_warcamp_direct_attack";
 				input.style.backgroundImage = "url(/game/img/fordern.gif)";
+				input.dataset.tooltip = '[[["'+ gca_locale.get("arena", "attack_player", {name:guildName}) +'","#fff;font-size:12px;"]]]';
 				form.appendChild(input);
 				td.appendChild(form);
 				element.appendChild(td);
 				
-				// Change life button
+				// See life button
 				let life_a = element.getElementsByTagName("a")[1];
 				life_a.textContent = "";
 				life_a.className = "gca_guild_warcamp_life";
+				life_a.dataset.tooltip = '[[["'+ life_points_translation +'","#fff;font-size:12px;"]]]';
+				
+				// Add Gladiatorius link
+				let td2 = document.createElement("td");
+				td2.style.textAlign = "center";
+				let gladiatorious_a = document.createElement("a");
+				gladiatorious_a.href = guild_link+"&submod=forumGladiatorius";
+				gladiatorious_a.textContent = "";
+				gladiatorious_a.className = "gca_guild_warcamp_gladiatorius";
+				td2.appendChild(gladiatorious_a);
+				element.insertBefore(td2, element.getElementsByTagName("td")[1]);
 			});
 		}
 	}
