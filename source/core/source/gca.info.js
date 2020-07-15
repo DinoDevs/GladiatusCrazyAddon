59845cc2b09ec9a512a5976d19fc9fc4bc9c15a5
Grammatopoulos Apostolos <greatapo@gmail.com>
2020-07-15 22:59:11 +0300

    Update gca.info.js

--- source/core/source/gca.info.js
+++ source/core/source/gca.info.js
@@ -8,7 +8,7 @@ var gca = {
 	name : "Gladiatus Crazy Addon",
 	shortName : "GCA",
 	description: "Improve your gladiatus experience!",
-	version: "4.3.1",
+	version: "4.3.2",
 	developers : [
 		"GreatApo",
 		"DarkThanos"
