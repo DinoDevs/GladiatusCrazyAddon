48465cdb23e92cf3fd27e2c5c1f6ba9b654edaac
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2021-01-05 18:02:20 +0200

    added scrolls check on guild scrolls

--- source/core/source/guild.storage.js
+++ source/core/source/guild.storage.js
@@ -14,6 +14,10 @@ var gca_guild_storage = {
 		(gca_options.bool("merchants","double_click_actions") &&
 			this.doubleClickActions.init());
 
+		// Run item categories actions
+		// TODO: Add options in settings
+		this.categories.check();
+
 		// Setting Link
 		gca_tools.create.settingsLink("guild");
 	},
@@ -94,6 +98,82 @@ var gca_guild_storage = {
 				gca_tools.item.move(this, 'inv');
 			}
 		}
+	},
+
+	// Categories
+	categories : {
+
+		// Check if storage has the category item
+		check : function() {
+			// Calculate number of items for each category
+			let items = [...document.querySelectorAll('#shop .ui-draggable')].reduce((info, item) => {
+				let basis = item.dataset.basis.match(/^(\d+)-(\d+)$/);
+				if (!basis) return info;
+				if (!info.hasOwnProperty(basis[1])) info[basis[1]] = 0;
+				info[basis[1]]++;
+				return info;
+			}, {});
+
+			// If scrolls
+			if (items[20] && items[20] > 0) {
+				this.scroll.loadData();
+			}
+		},
+
+		// Scrolls Category
+		scroll : {
+			loadData : function(){
+				gca_tools.ajax.cached.known_scrolls().then(
+					(result) => {
+						// Save lists
+						this.prefix = result.id.prefix;
+						this.suffix = result.id.suffix;
+
+						// Check scrolls
+						this.showIsLearned();
+					},
+					() => {
+						// On error
+						//setTimeout(() => {
+						//	this.loadData();
+						//}, 10 * 1000);
+					}
+				);
+			},
+			// Apply
+			showIsLearned : function(){
+				// If no data return
+				if(!this.prefix) return;
+
+				// RegExp to check is item is scroll
+				var isScroll = new RegExp(/^20-/);
+
+				// For each item
+				jQuery("#shop .ui-draggable").each((i, item) => {
+					// If not a scroll, return
+					if(!isScroll.test(item.dataset.basis)) return;
+
+					// If already parsed
+					if(item.dataset.gcaFlag_isLearned) return;
+					// Flag as parsed
+					item.dataset.gcaFlag_isLearned = true;
+
+					// Get hash
+					let hash = gca_tools.item.hash(item);
+					if (!hash) return;
+					// Check if own
+					let known = (this.prefix.indexOf(hash.prefix) >= 0 || this.suffix.indexOf(hash.suffix) >= 0);
+					if (known) {
+						item.style.filter = "drop-shadow(2px 2px 1px rgba(255,0,0,0.4)) drop-shadow( 2px -2px 1px rgba(255,0,0,0.4)) drop-shadow(-2px -2px 1px rgba(255,0,0,0.4)) drop-shadow(-2px 2px 1px rgba(255,0,0,0.4))";
+						jQuery(item).data("tooltip")[0].push([gca_locale.get("packages","known_scroll"), "red"]);
+					}
+					else {
+						item.style.filter = "drop-shadow(2px 2px 1px rgba(0,255,0,0.4)) drop-shadow( 2px -2px 1px rgba(0,255,0,0.4)) drop-shadow(-2px -2px 1px rgba(0,255,0,0.4)) drop-shadow(-2px 2px 1px rgba(0,255,0,0.4))";
+						jQuery(item).data("tooltip")[0].push([gca_locale.get("packages","unknown_scroll"), "green"]);
+					}
+				});
+			}
+		}
 	}
 };
 
@@ -114,5 +194,5 @@ var gca_guild_storage = {
 })();
 
 // ESlint defs
-/* global gca_options, gca_tools */
+/* global gca_locale, gca_options, gca_tools */
 /* global jQuery */
