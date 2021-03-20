557440532a0e9777a4e20e839fc96c3176507261
Grammatopoulos Athanasios-Vasileios <darkthanos.gr@gmail.com>
2021-03-20 15:06:01 +0200

    Update de.js

--- source/core/locale/de.js
+++ source/core/locale/de.js
@@ -86,10 +86,10 @@ gca_languages['de'] = {
 			family_friends : "Familien Freunde",
 
 			// Guild donate
-			donate_gold_confirm : "Bist du sicher, dass du {{number}} gold spenden möchtest?",
+			donate_gold_confirm : "Bist du sicher, dass du {{number}} Gold spenden möchtest?",
 			donate_gold_success : "Dein Gold wurde gespendet",
-			donate_gold_failed : "Deine Goldspende schlug fehlt",
-			donate_gold_no_gold : "Kein Gold zum spenden vorhanden",
+			donate_gold_failed : "Deine Goldspende schlug fehl",
+			donate_gold_no_gold : "Kein Gold zum Spenden vorhanden",
 			donate_gold_all_gold : "Gesamtes Gold spenden",
 
 			// Quest timer
@@ -108,7 +108,7 @@ gca_languages['de'] = {
 
 
 			// Gold - Exp data
-			
+			gold_exp_data : "Gold- und Erfahrungswerte",
 			gold_exp_data_today : "Die letzten 24 Stunden",
 			gold_exp_data_week : "Die letzten 7 Tage",
 			gold_exp_data_avg_day : "Durchschnittswerte pro Tag",
@@ -119,7 +119,7 @@ gca_languages['de'] = {
 			gold_exp_data_total_gold : "gesamtes Gold",
 			
 			// Items
-			
+			merchenary_type : "Typ: {{name}} ({{number}})"
 		},
 
 		// Overview
@@ -132,9 +132,9 @@ gca_languages['de'] = {
 
 			// More player info
 			more_player_info : "Weitere Informationen zum Spieler",
-			can_use_max_item_level : "Kann Gegenstände bis zum Level {{max}} benutzen.",
-			can_see_market_max_item_level : "Kann Gegenstände am Markt bis zum Level {{max}} sehen.",
-			can_see_auction_item_levels : "Kann Gegenstände im Auktionshaus von Level {{min}} bis Level {{max}} sehen."
+			can_use_max_item_level : "Kann Gegenstände bis zur Stufe {{max}} benutzen.",
+			can_see_market_max_item_level : "Kann Gegenstände am Markt bis zur Stufe {{max}} sehen.",
+			can_see_auction_item_levels : "Kann Gegenstände im Auktionshaus von Stufe {{min}} bis {{max}} sehen."
 		},
 
 		// Pantheon section
@@ -156,7 +156,7 @@ gca_languages['de'] = {
 
 			// Medic
 			medic_lost_points : "Verlorene Punkte",
-			medic_points_to_heal : "Punkte zum heilen",
+			medic_points_to_heal : "Punkte zum Heilen",
 			medic_life_after_heal : "Leben nach dem heilen"
 		},
 
@@ -165,8 +165,36 @@ gca_languages['de'] = {
 			material_drop_chance : "{{number}}% Chance das Schmiedematerial zu erhalten"
 		},
 
+		// Arena section
+		arena : {
+			global_arena_title : "Globale Arena",
+			global_arena_description : "Das ist die ultimative Arena, in der Gladiatoren aus aller Welt zusammenkommen! In dieser Arena kämpfen Gladiatoren nicht um Gold oder Erfahrung, sondern um einen Platz auf der Weltspitzenliste!",
+			
+			global_highscore : "Globale Bestenliste",
+			country : "Land",
+			server : "Server",
+			target_list : "Liste der Angriffsziele",
+			target_list_add : "Angriffsziel hinzufügen",
+			target_list_remove : "Angriffsziel löschen",
+			error_sth_went_wrong : "Etwas ist schief gelaufen",
+			error_response : "Die Antwort vom Server zeigt an, dass ein Fehler aufgetreten ist.",
+			error_blocked_access : "Etwas blockiert den Zugang zum GCA Server ({{url}})",
+			error_connection : "Verbindungsfehler",
+			attack_player : "Klicke um “{{name}}” anzugreifen",
+			fight_won : "Du hast den Kampf gewonnen!",
+			fight_lost : "Du hast den Kampf verloren...",
+			player_tired : "Du hast gerade einen Kampf in der Arena bestritten",
+			player1_hits_player2 : "{{name1}} schlägt {{name2}}",
+			player_takes_x_damage :"{{name}} erleidet {{number}} Schaden",
+			player_dies :"{{name}} gestorben"
+		},
+
 		// Training section
 		training : {
+			// Points analysis
+			stats_points : "Trainingsstatistik",
+			points_breakdown : "Points Breakdown",
+			
 			// Cost calculator
 			total_cost : "Gesamtkosten",
 			// Discount show
@@ -176,7 +204,7 @@ gca_languages['de'] = {
 		// Auction section
 		auction : {
 			// Info
-			items_info : "Gegenstands Informationen",
+			items_info : "Informationen zu Gegenstände",
 			// Number of items in the page
 			number_of_items : "Anzahl Gegenstände : {{number}}",
 			// Number of items that have been bidden in the page
@@ -213,6 +241,12 @@ gca_languages['de'] = {
 			show_hide_doll : "Zeige / Verstecke Ausrüstung des Charakters"
 		},
 		
+		// Merchants
+		merchants : {
+			search_item_in_merchants : "Suche Gegenstand beim Händler",
+			no_such_item : "Gesuchter Gegenstand nicht gefunden."
+		},
+		
 		// Packages
 		packages : {
 			event_items : "Event Gegenstände",
@@ -221,6 +255,24 @@ gca_languages['de'] = {
 			advance_filters : "Erweiteter Filter",
 			advance_filters_apply : "Filter anwenden",
 			advance_filters_clear : "Filter zurücksetzen",
+			advance_filters_found : "(Gefunden:{{items}})"
+		},
+		
+		// Report
+		reports : {
+			avg_damage : "Ø-Schaden",
+			avg_heal : "Ø-Heilung",
+			total_hits : "Treffer gesamt",
+			hits : "Treffer",
+
+			points : "Punkte"
+		},
+
+		// Cross-Browser Sync
+		sync : {
+			are_you_sure : "Möchtest du dich als Spieler {{name}} anmelden?",
+			gladiatus_crazy_addon_dependency : "Sie müssen Gladiatus Crazy Addon auf dem anderen Browser installiert haben.",
+			how_to_sync_info : "Kopiere die URL und fügen diese in den anderen Browser ein oder scannen Sie den QR-Code."
 		},
 
 		// Settings
@@ -252,7 +304,7 @@ gca_languages['de'] = {
 			category_data : "Daten",
 
 			// Settings - Global
-			category_global$language_select : "Add on Sprache ändern",
+			category_global$language_select : "Add-on Sprache ändern",
 			category_global$sound_notifications : "Aktiviere Ton Benachrichtigungen für Missionen, Dungeons und Arenas",
 			category_global$browser_notifications : "Aktiviere Browser Benachrichtigungen",
 			category_global$extended_hp_xp_info : "Zeige erweiterte HP und XP Infos",
@@ -268,10 +320,10 @@ gca_languages['de'] = {
 			category_global$submenu_click_to_change : "Wechsel das Untermenü per Klick",
 			category_global$remember_tabs : "Merke Händler und Inventar Tabs",
 			category_global$attacked_timers : "Anzeige der letzten Angriffe aktivieren",
-			category_global$quest_timer : "Aufgaben timer anzeigen",
+			category_global$quest_timer : "Aufgaben Timer anzeigen",
 			category_global$merchants_timer : "Händlerzeit anzeigen",
 			category_global$forge_timers : "Schmiede/Schmelzzeit anzeigen",
-			category_global$cooldown_sound_notifications : "Aktiviere sound Benachrichtigungen (Expedition, Dungeon, Arena)",
+			category_global$cooldown_sound_notifications : "Aktiviere Sound Benachrichtigungen (Expedition, Dungeon, Arena)",
 			category_global$notify_new_guild_application : "Benachrichtigen bei einer neuen Gildenbewerbung",
 			category_global$notify_new_guild_application_interval : "Überprüfe auf neue Bewerbung (Minuten)",
 			category_global$notify_guild_attack_ready : "Benachrichtige wenn Abklingzeit des Gildenkampfes vorüber ist",
@@ -280,8 +332,8 @@ gca_languages['de'] = {
 			category_global$item_shadow : "Qualität der Gegenstände als Schatten anzeigen",
 			category_global$inventory_options_group : "Inventar Optionen zusammenfassen",
 			category_global$inventory_gold_info : "Gold des Inventars anzeigen",
-			
-			category_global$gold_exp_data : "Zeige Gold und Erfahrung Daten an",
+			category_global$pagination_layout : "Ändere Layout der Seitennummerierung (Nachrichten, Pakete,...)",
+			category_global$gold_exp_data : "Zeige Gold- und Erfahrungswerte an",
 			category_global$pray_shorcut : "Beten Tasten in der Unterwelt anzeigen",
 			category_global$centurio_powerups_timers : "Zeige Timer zu Centurio und Pakte an der Premium Schaltfläche",
 			category_global$show_durability : "Haltbarkeit der Gegenstände unten links anzeigen",
@@ -295,7 +347,7 @@ gca_languages['de'] = {
 
 			category_overview$best_food : "Hebt das beste Essen hervor",
 			category_overview$overfeed_food : "Blende Essen aus, dass zuviel heilt",
-
+			category_overview$double_click_consume : "Durch Doppelklick Gegenstände konsumieren",
 			category_overview$daily_bonus_log : "Tagesbonus protokollieren",
 			category_overview$buffs_detailed_time : "Zeige detaillierte Timer für Buffs (Sekunden)",
 			category_overview$mercenaries_manager : "Söldnermanager anzeigen",
@@ -308,7 +360,7 @@ gca_languages['de'] = {
 			category_messages$messages_layout : "Füge Nachrichteninterface hinzu",
 			category_messages$show_unread : "Ungelesene Nachrichten hervorheben",
 			category_messages$separate_days : "Teile Nachrichten nach Tagen auf",
-			category_messages$send_message_box : "Aktiviere die Nachricht senden box",
+			category_messages$send_message_box : "Aktiviere die Nachricht Senden-Box",
 			category_messages$more_guild_mate_info : "Zeige zusätzliche Mitgliederinformationen",
 			category_messages$show_message_links : "Links anzeigen",
 			category_messages$get_guild_battle_info : "Zeige Gildenkämpfe direkt an",
@@ -327,7 +379,7 @@ gca_languages['de'] = {
 			category_packages$special_category_features : "Aktiviere spezielle Funktionen\n•Zeigt ob Schriftrollen bereits erlernt wurden\n•Zeigt nicht erlernte Suffix / Präfix via Symbol am Gegenstand an",
 			category_packages$double_click_open : "Pakete durch Doppelklick öffnen",
 			category_packages$advance_filter : "Erweiteter Pakete Filter",
-
+			category_packages$pop_over_bag : "Beim Scrollen Inventar hervorheben",
 			// Settings - Pantheon
 			category_pantheon$quests_reorder : "Aktiviere die Gruppierung von Aufgaben",
 			category_pantheon$quests_detailed_rewards : "Zeige detaillierte Aufgabenbelohnungen ",
@@ -369,10 +421,10 @@ gca_languages['de'] = {
 			// Settings - Market
 			category_market$soulbound_warning : "Kaufbestätigung für seelengebundene Gegenstände",
 			category_market$one_gold_warning : "Kaufbestätigung für Gegenstände, die 1 Gold kosten",
-			category_market$cancel_all_button : "Zeige Alle-abbrechen-Taste",
-			category_market$remember_sell_duration : "Merken die zuletzt gewählte Verkaufsdauer",
+			category_market$cancel_all_button : "Zeige Alle-Abbrechen-Taste",
+			category_market$remember_sell_duration : "Merke die zuletzt gewählte Verkaufsdauer",
 			category_market$sell_duration : "Wähle die Standardverkaufsdauer aus",
-
+			category_market$one_gold_mode : "Schaltfläche zum Ändern des Marktpreise immer auf 1 Gold",
 			category_market$remember_sort : "Die letzte Sortierreihenfolge merken",
 			category_market$double_click_select : "Wähle Gegenstände durch Doppelklick aus",
 			category_market$sell_warning_icons : "Warnsymbol bei Verkauf von Seelengebundene Gegenstände",
@@ -392,7 +444,7 @@ gca_languages['de'] = {
 			// Settings - Auction
 			category_auction$items_counters : "Zeige Anzahl der Gegenstände",
 			category_auction$more_search_levels : "Zeige mehr Stufen in den Suchoptionen",
-			category_auction$item_price_analyze : "Analisiere die Preise der Gegenstände",
+			category_auction$item_price_analyze : "Analysiere die Preise der Gegenstände",
 			category_auction$item_level : "Zeige Level der Gegenstände",
 			category_auction$x3_items_per_line : "Zeige 3 Gegenstände pro Reihe",
 			category_auction$multi_bids : "mehrere Gebote ohne Seitenaktualisierung",
@@ -408,10 +460,10 @@ gca_languages['de'] = {
 			// Settings - Data
 			category_data$export_settings : "Exportieren von Einstellungsdaten in eine Datei",
 			category_data$import_settings : "Importieren der Einstellungsdaten aus einer Datei",
-			category_data$reset_settings : "Setzt die Einstellungen des Addons zurück",
-			category_data$clear_data : "Lösche alle Addon Daten",
-			category_data$clear_cache_data : "Lösche Cache Dateien des Addons",
-
+			category_data$reset_settings : "Setzt die Einstellungen des Add-ons zurück",
+			category_data$clear_data : "Lösche alle Add-on Daten",
+			category_data$clear_cache_data : "Lösche Cache Dateien des Add-ons",
+			category_data$cross_browser_login : "Browserübergreifende Anmeldesynchronisation",
 
 
 			// Buttons
@@ -422,15 +474,15 @@ gca_languages['de'] = {
 			clear : "löschen",
 			do_not_show : "nicht anzeigen",
 			show_as : "anzeigen als",
-
-
-
-
+			show_info : "Zeige Informationen",
+			each_category : "Zeige pro Kategorie",
+			all_category : "Zeige pro Kategorie und gesamten Inhalt",
+			do_not_run : "Nicht anzeigen",
 
 			// Info
 			translated_percent : "Aktuell übersetzt: {{number}}%",
 			translated_by : "Übersetzt von: {{string}}",
-			reset_settings_confirm : "Möchtest du die Einstellungen des Addons wirklich zurücksetzen?",
+			reset_settings_confirm : "Möchtest du die Einstellungen des Add-ons wirklich zurücksetzen?",
 			clear_data_confirm : "Möchtest du wirklich alle Daten des Addons löschen?",
 			data_exported_save_the_file : "Datei wurde exportiert. Speicher die Datei.",
 			missing_translations : "Fehlende Übersetzung",
