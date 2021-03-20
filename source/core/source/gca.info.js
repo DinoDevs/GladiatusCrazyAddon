29982da014160e39b6791765696422a00de1080c
Grammatopoulos Athanasios-Vasileios <darkthanos.gr@gmail.com>
2021-03-20 15:12:10 +0200

    Changed version for beta

--- source/core/source/gca.info.js
+++ source/core/source/gca.info.js
@@ -8,7 +8,7 @@ var gca = {
 	name : "Gladiatus Crazy Addon",
 	shortName : "GCA",
 	description: "Improve your gladiatus experience!",
-	version: "4.3.4",
+	version: "4.3.5",
 	developers : [
 		"GreatApo",
 		"DarkThanos"
