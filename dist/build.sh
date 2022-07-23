5aeb113e8e1b7a4b466a34f312a9f1bed2e0ea17
gramthanos <gramthanos@gmail.com>
2022-07-23 22:19:08 +0300

    Added draft Firefox deployment code

--- dist/build.sh
+++ dist/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm GladiatusCrazyAddOn_Chrome.zip
-rm GladiatusCrazyAddOn_Chrome_Manifest_v2.zip
+rm -f GladiatusCrazyAddOn_Chrome.zip
+rm -f GladiatusCrazyAddOn_Chrome_Manifest_v2.zip
+rm -f GladiatusCrazyAddOn_Firefox.xpi
 
 cd ../source
 
@@ -9,7 +10,11 @@ echo "Building for Chrome (Manifest v3)"
 zip -q -r ../dist/GladiatusCrazyAddOn_Chrome.zip core icons init.js manifest_v3.json
 printf "@ manifest_v3.json\n@=manifest.json\n" | zipnote -w ../dist/GladiatusCrazyAddOn_Chrome.zip
 
-echo "Building for Chrome (Manifest v2)"
+echo "Building for Chrome (Deprecated Manifest v2)"
 zip -q -r ../dist/GladiatusCrazyAddOn_Chrome_Manifest_v2.zip core icons init.js manifest.json
+
+echo "Building for Firefox (Manifest v2)"
+zip -q -r ../dist/GladiatusCrazyAddOn_Firefox.zip core icons init.js manifest.json
+mv ../dist/GladiatusCrazyAddOn_Firefox.zip ../dist/GladiatusCrazyAddOn_Firefox.xpi
 
 cd ../dist
