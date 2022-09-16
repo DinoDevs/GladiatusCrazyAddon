ff7694fad9f1c49fa96afd415cbc1cb8ef580d95
gramthanos <gramthanos@gmail.com>
2022-09-16 14:40:48 +0300

    Developer version increased to v4.3.8

--- source/core/source/gca.info.js
+++ source/core/source/gca.info.js
@@ -8,7 +8,7 @@ var gca = {
 	name : "Gladiatus Crazy Addon",
 	shortName : "GCA",
 	description: "Improve your gladiatus experience!",
-	version: "4.3.7",
+	version: "4.3.8",
 	developers : [
 		"GreatApo",
 		"DarkThanos"
