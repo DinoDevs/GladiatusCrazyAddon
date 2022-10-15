a2b8c6dcb5783a2f792772ecd66533286ef285f9
FrutyX <23278552+FrutyX@users.noreply.github.com>
2022-10-15 11:36:53 +0200

    Just some magic #2

--- source/core/source/settings.js
+++ source/core/source/settings.js
@@ -648,8 +648,8 @@ var gca_settings = {
 				"gold_exp_data" : true,
 				
 				// Underworld
-					// Pray Shortcut
-					"pray_shorcut" : true,
+				// Pray Shortcut
+				pray_shorcut" : true,
 				
 				// Centurion & PowerUps timers
 				"centurio_powerups_timers" : false,
@@ -743,10 +743,10 @@ var gca_settings = {
 				"hide_language_flags" : false,
 				
 				// Merge menu merchants
-		        "menu_merge_merchants" : false,
+		                "menu_merge_merchants" : false,
 				
 				// Merge menu merchants
-		        "menu_merge_items" : false,
+		                "menu_merge_items" : false,
 				
 				// Hide expedition button
 				"bar_hide_exp_btn" : false,
