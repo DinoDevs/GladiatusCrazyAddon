4b937d67dddf497284396eefac0f4d6d86b96c28
FrutyX <23278552+FrutyX@users.noreply.github.com>
2025-01-21 16:01:04 +0100

    Highlight duplicates - settings.js

--- source/core/source/settings.js
+++ source/core/source/settings.js
@@ -1197,6 +1197,8 @@ var gca_settings = {
 				"tooltips_qualty_white" : false,
 				// Add quality symbols on tooltips
 				"tooltips_qualty_symbols" : false,
+				// Highlight item duplicates
+				"highlight_item_duplicates : false
 			},
 
 			"events" : {
