ad4442d0323f5b10def0fb90e0f9eccba4363532
FrutyX <23278552+FrutyX@users.noreply.github.com>
2025-01-25 00:34:09 +0100

    Gods cds - overall improvements

--- source/core/source/global.js
+++ source/core/source/global.js
@@ -2974,12 +2974,18 @@ var gca_global = {
 					const wrapper = document.createElement('div');
 					wrapper.style.position = 'absolute'; 
 					wrapper.style.top = '200px';  
-					wrapper.style.right = '-160px'; 
+					wrapper.style.right = '-30px'; 
 					wrapper.style.maxWidth = '150px';
 					wrapper.style.zIndex = '899'; 
 					wrapper.style.overflow = 'hidden';
 					wrapper.style.whiteSpace = 'normal';
 					wrapper.style.wordWrap = 'break-word';
+					wrapper.style.background = 'linear-gradient(to bottom, #f7e7ce, #e4c593)';
+					wrapper.style.border = '1px solid #6b1c15';
+					wrapper.style.borderWidth = '4px 1px';
+					wrapper.style.borderRadius = '6px';
+					wrapper.style.padding = '2px';
+				
 
 					// Title
 					const title = document.createElement('h2');
@@ -3027,7 +3033,7 @@ var gca_global = {
 					wrapper.appendChild(section);
 			
 					// Insert on page
-					document.getElementById('content').appendChild(wrapper);
+					document.getElementById('header_game').appendChild(wrapper);
 				} catch (error) {
 					console.error("Error fetching gods data:", error);
 				}
