32d4790c6331b360a533a89524f5b0de3a40e62a
FrutyX <23278552+FrutyX@users.noreply.github.com>
2025-04-24 15:53:31 +0200

    Guild storage scrolls fix (#532)

--- source/core/source/guild.storage.js
+++ source/core/source/guild.storage.js
@@ -217,7 +217,7 @@ var gca_guild_storage = {
 		// Check if storage has the category item
 		check : function() {
 			// Calculate number of items for each category
-			let items = [...document.querySelectorAll('#shop .ui-draggable')].reduce((info, item) => {
+			let items = [...document.querySelectorAll('#shop .ui-draggable, #shop .ui-droppable')].reduce((info, item) => {
 				let basis = item.dataset.basis.match(/^(\d+)-(\d+)$/);
 				if (!basis) return info;
 				if (!info.hasOwnProperty(basis[1])) info[basis[1]] = 0;
@@ -260,7 +260,7 @@ var gca_guild_storage = {
 				var isScroll = new RegExp(/^20-/);
 
 				// For each item
-				jQuery("#shop .ui-draggable").each((i, item) => {
+				jQuery("#shop .ui-draggable, #shop .ui-droppable[class*=\"item-i-\"]").each((i, item) => {
 					// If not a scroll, return
 					if(!isScroll.test(item.dataset.basis)) return;
 
